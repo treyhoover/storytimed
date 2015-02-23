b6e001245cff53375c7adfeebe815eaf738f7418
Trey Hoover <thoover.mail@gmail.com>
2015-02-23 17:29:09 -0500

    hardcode node api

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 var express = require('express');
 
 var app = express();
-var path = require('path')
+var path = require('path');
 
 app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.render('index', { title: 'Hey', message: 'Hello there!'});
-})
+});
+
+app.get('/story/:id', function(req, res) {
+    // req.params.id returns :id
+    res.json({
+        storyPoints: [
+            {author: 'Trey', body: 'Once upon a time there was a group of aspiring developers.'},
+            {author: 'Aurora', body: 'They thought they were signing up for code school, but they were signing up for so much more.'},
+            {author: 'Andy', body: 'Little did they know, their lives would soon be at stake.'},
+            {author: 'Emily', body: 'And so it began: The Coder Games.'}
+        ]
+    });
+});
 
 var server = app.listen(3000, function () {
-
-    var host = 'localhost';
-    var port = '3000';
-
-    console.log('Example app listening at http://%s:%s', host, port)
-
+    console.log('Example app listening at http://localhost:3000');
 });
