9a61e9990cf279740c7d4a6a374be21101f3df23
Trey Hoover <thoover.mail@gmail.com>
2015-02-27 02:36:58 -0500

    remove debug code

--- public/js/app.js
+++ public/js/app.js
@@ -74,9 +74,7 @@
         this.point = {};
 
         socket.on('new storyPoint', function(msg){
-            //console.log('new storyPoint!', msg);
             story.points.push(msg);
-            //console.log('last story point:', story.points[story.points.length - 1]);
             $scope.$apply();
             $('.story').scrollTop($('.story')[0].scrollHeight);
         });
@@ -97,8 +95,6 @@
                     }
                 }
             }).success(function (data, status, headers, config) {
-                //console.log(data);
-                //story.points.push(story.point);
                 story.point = {};
             }).error(function (data, status, headers, config) {
                 console.log(data);
@@ -107,7 +103,6 @@
 
         $http.get('/api/story/show/1')
             .success(function(response){
-                //console.log("Successfully fetched story points", response.storyPoints);
                 story.points = response.storyPoints;
             });
     }]);
